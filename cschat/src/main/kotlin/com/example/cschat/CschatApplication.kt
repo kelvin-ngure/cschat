8067afad41e88e0e72e96f95f461f672b612cb3a
kelvin-ngure <kelvin.1.ngure@gmail.com>
2024-02-26 14:05:21 +0300

    Set up spring project - Add dependencies

---
+++ cschat/src/main/kotlin/com/example/cschat/CschatApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cschat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CschatApplication
+
+fun main(args: Array<String>) {
+	runApplication<CschatApplication>(*args)
+}
---
+++ cschat/src/test/kotlin/com/example/cschat/CschatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cschat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CschatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

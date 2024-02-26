b56e1cb3ba6c1a5210c868364c48a50e55db7290
kelvin-ngure <kelvin.1.ngure@gmail.com>
2024-02-26 22:37:36 +0300

    Create landing page that allows creation of customer and agent profiles

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        blue: {
+          100: "#A7E6FF",
+          101: "#102933",
+          102: "#9ed9f0",
+          103: "#66b0cc",
+          500: '#0070F3',
+        },
+        green: {
+          100: "#25D366"
+        }
+      },
     },
   },
   plugins: [],
